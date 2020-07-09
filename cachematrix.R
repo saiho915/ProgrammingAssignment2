633bdec7ce698d373b78c084db0d47735d1c9882
saiho915 <yshshihao@gmail.com>
2020-07-09 23:31:41 +0800

    second try

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,14 @@
 
 ## Write a short comment describing this function
 
+## Person Explanation:
+## 1: assign an initial value to i(stands for inverse)
+## 2(set): refresh x
+## 3(get): return x
+## 4(setinverse): assign the inverse to i
+## 4(getinverse): return inverse
+## 6(list): return a list containing related functions
+
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
   set <- function(y) {
@@ -19,6 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+
+## Person Explanation:
+## 1: extract the initial value for testing
+## 2: test whether inverse exists or not
+## 3: get the value of x
+## 4: calculate the inverse of the matrix
+## 5: assign the calculated inverse into the variable i
+## 6: return the calculated inverse
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
